bad2fddab703a81449e061414e377812c18e2f3d
Wojciech Zięba <wojciech.zieba@protonmail.com>
2020-03-30 04:11:54 +0200

    Androidx migration (#2)
    * config version bump
    
    * Automatic migration
    
    * dependencies version bump
    
    Co-authored-by: Mateusz Kwieciński <mateusz.kwiecinski@tooploox.com>

--- src/main/java/com/buglife/sdk/InvocationMethodManager.java
+++ src/main/java/com/buglife/sdk/InvocationMethodManager.java
@@ -22,8 +22,8 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import java.io.File;
 
